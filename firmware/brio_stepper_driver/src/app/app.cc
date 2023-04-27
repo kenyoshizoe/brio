1c8155301f11e6460e8961a70a28363d05c008b7
Ken Yoshizoe <kenyoshizoe@gmail.com>
2023-04-27 20:31:14 +0900

    :rocket: parameter tuning

--- firmware/brio_stepper_driver/src/app/app.cc
+++ firmware/brio_stepper_driver/src/app/app.cc
@@ -26,7 +26,14 @@ void MainTask() {
                              STEPPER1_DIR_GPIO_Port, STEPPER1_DIR_Pin,
                              STEPPER1_SENS_GPIO_Port, STEPPER1_SENS_Pin, &htim3,
                              TIM_CHANNEL_3);
-  stepper1->SetMicrostep(4);
+  stepper1->SetMicrostep(16);
+  stepper1->SetGearRatio(5);
+  stepper1->SetInitialSpeed(0.1 * brio::kPI);
+  stepper1->SetDefaultSpeed(1 * brio::kPI);
+  stepper1->SetAccel(0.5 * brio::kPI);
+  stepper1->SetMinRad(-2 * brio::kPI);
+  stepper1->SetMaxRad(2 * brio::kPI);
+  stepper1->SetSensReverse(true);
   stepper2 = new brio::A4988(STEPPER2_STEP_GPIO_Port, STEPPER2_STEP_Pin,
                              STEPPER2_DIR_GPIO_Port, STEPPER2_DIR_Pin,
                              STEPPER2_SENS_GPIO_Port, STEPPER2_SENS_Pin, &htim2,
@@ -40,7 +47,13 @@ void MainTask() {
                              STEPPER3_DIR_GPIO_Port, STEPPER3_DIR_Pin,
                              STEPPER3_SENS_GPIO_Port, STEPPER3_SENS_Pin,
                              &htim15, TIM_CHANNEL_1);
-  stepper3->SetMicrostep(4);
+  stepper3->SetMotorSteps(200);
+  stepper3->SetMicrostep(16);
+  stepper3->SetGearRatio(3);
+  stepper3->SetInitialSpeed(0.2 * brio::kPI);
+  stepper3->SetDefaultSpeed(2 * brio::kPI);
+  stepper3->SetAccel(2 * brio::kPI);
+  stepper3->SetSensReverse(true);
   HAL_GPIO_WritePin(STEPPER_RESET_GPIO_Port, STEPPER_RESET_Pin, GPIO_PIN_RESET);
   HAL_Delay(10);
   HAL_GPIO_WritePin(STEPPER_RESET_GPIO_Port, STEPPER_RESET_Pin, GPIO_PIN_SET);
@@ -51,13 +64,17 @@ void MainTask() {
   HAL_TIM_Base_Start_IT(&htim3);
   HAL_TIM_Base_Start_IT(&htim6);
   HAL_TIM_Base_Start_IT(&htim15);
-
-  SEGGER_RTT_printf(0, "Stepper motor driver initialized.\r\n");
-
+  // Return to origin
+  SEGGER_RTT_printf(0, "Stepper motor 1 return to origin...");
+  stepper1->ReturnToOrigin();
+  SEGGER_RTT_printf(0, "done.\r\n");
   SEGGER_RTT_printf(0, "Stepper motor 2 return to origin...");
   stepper2->ReturnToOrigin();
   SEGGER_RTT_printf(0, "done.\r\n");
-  stepper2->MoveTo(4 * brio::kPI, 8 * brio::kPI);
+  SEGGER_RTT_printf(0, "Stepper motor 3 return to origin...");
+  stepper3->ReturnToOrigin();
+  SEGGER_RTT_printf(0, "done.\r\n");
+  SEGGER_RTT_printf(0, "Stepper motor driver initialized.\r\n");
 
   HAL_Delay(1000);
 
@@ -65,10 +82,6 @@ void MainTask() {
   cmd_queue.clear();
 
   while (true) {
-    if (stepper2->IsRunning()) {
-      continue;
-    }
-
     int input = SEGGER_RTT_GetKey();
     if (input < 0) {
       continue;
@@ -79,28 +92,49 @@ void MainTask() {
     cmd_queue.push(input);
 
     if (cmd_queue.back() == ';') {
+      if (cmd_queue.size() < 3) {
+        cmd_queue.clear();
+        continue;
+      }
+
+      // Parse command
       char cmd = cmd_queue.front();
       cmd_queue.pop();
-      char value_char[256] = "";
+      char target = cmd_queue.front();
+      cmd_queue.pop();
+      char value_char[16] = "";
       for (int i = 0; i < cmd_queue.size() - 1; i++) {
         strcat(value_char, &cmd_queue.front());
         cmd_queue.pop();
       }
       int value = atoi(value_char);
       cmd_queue.clear();
-      SEGGER_RTT_printf(0, "Command: %c, Value: %d\r\n", cmd, value);
+
+      SEGGER_RTT_printf(0, "Command: %c, Target: %c, Value: %d\r\n", cmd,
+                        target, value);
+
+      brio::A4988* stepper;
+      if (target == '1') {
+        stepper = stepper1;
+      } else if (target == '2') {
+        stepper = stepper2;
+      } else if (target == '3') {
+        stepper = stepper3;
+      } else {
+        continue;
+      }
 
       if (cmd == 'm') {
         // Move to
-        stepper2->MoveTo(value * brio::kPI);
+        stepper->MoveTo(0.1 * value * brio::kPI);
       }
       if (cmd == 's') {
         // Set speed
-        stepper2->SetDefaultSpeed(value * brio::kPI);
+        stepper->SetDefaultSpeed(0.1 * value * brio::kPI);
       }
       if (cmd == 'a') {
         // Acceleration
-        stepper2->SetAccel(value * brio::kPI);
+        stepper->SetAccel(0.1 * value * brio::kPI);
       }
       if (cmd == 'r') {
         HAL_NVIC_SystemReset();
