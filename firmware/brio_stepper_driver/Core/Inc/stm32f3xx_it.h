31744aaa297a0509af5b710f5dab4ead73ef4956
Ken Yoshizoe <kenyoshizoe@gmail.com>
2023-04-28 12:19:57 +0900

    :sparkles: change cubemx spi settting

--- firmware/brio_stepper_driver/Core/Inc/stm32f3xx_it.h
+++ firmware/brio_stepper_driver/Core/Inc/stm32f3xx_it.h
@@ -55,6 +55,8 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
+void DMA1_Channel2_IRQHandler(void);
+void DMA1_Channel3_IRQHandler(void);
 void TIM1_BRK_TIM15_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void TIM3_IRQHandler(void);
